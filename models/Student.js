058cc2caaad82afc455aa659db62a944bef4e2ff
Aman <amanvarshney.varshney@gmail.com>
2023-02-19 04:42:48 +0530

    Created Student Schema

---
+++ models/Student.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const studentSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String, 
+        required: true
+    },
+    batch: {
+        type: Number,
+        required: true
+    },
+    college: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["Placed", "Not Placed"],
+        default: "Not Placed"
+    },
+    scores: {
+        dsa: {
+            type: Number,
+            required: true
+        },
+        webd: {
+            type: Number,
+            required: true
+        },
+        react: {
+            type: Number,
+            required: true
+        }
+    }
+}, {
+    timestamps: true
+});
+
+const Student = mongoose.model('Student', studentSchema);
+
+module.exports = Student;
