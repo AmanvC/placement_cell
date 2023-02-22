7dab3571e6b814b5df9f497f1db74a195618e2f6
Aman <amanvarshney.varshney@gmail.com>
2023-02-23 02:25:48 +0530

    Added styling to login page

---
+++ assets/js/login.js
@@ -0,0 +1,5 @@
+const signupContainer = document.getElementById('signup-container');
+
+document.querySelector('.actions a').addEventListener('click', () => {
+    signupContainer.style.display = 'block';
+})
